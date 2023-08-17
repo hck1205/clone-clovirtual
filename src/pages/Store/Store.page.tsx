1707bae1efbf3aff448c7988c2f1d49dee837c28
CkHong <hck1205@daum.net>
2023-08-18 00:56:05 +0900

    updated infinite scroll

--- src/pages/Store/Store.page.tsx
+++ src/pages/Store/Store.page.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react';
 import { useFetchStore } from '@/hook/useFetchStore';
 import { Spinner } from '@/components';
 import Item from '@/components/Item';
 import Header from '@/components/Header';
+import { useInView } from 'react-intersection-observer';
 
 import { StorePageWrapper, GridWrapper } from './Store.styled';
 
+const ITEMS_PER_PAGE = 15;
+
 function Store() {
   const { storeData, isFetching } = useFetchStore();
+  const [bottomRef, inView] = useInView();
+
+  const [offset, setOffSet] = useState(1);
+
+  useEffect(() => {
+    if (inView) {
+      setOffSet((prev) => prev + 1);
+    }
+  }, [inView]);
 
   return (
     <StorePageWrapper>
@@ -18,9 +31,14 @@ function Store() {
         </div>
       ) : (
         <GridWrapper>
-          {storeData.map((data) => {
-            return <Item key={data.id} itemData={data} />;
-          })}
+          {storeData
+            .filter((_data, index) => index < ITEMS_PER_PAGE * offset)
+            .map((data) => {
+              return <Item key={data.id} itemData={data} />;
+            })}
+          <div ref={bottomRef} style={{ color: 'white' }}>
+            test
+          </div>
         </GridWrapper>
       )}
     </StorePageWrapper>
